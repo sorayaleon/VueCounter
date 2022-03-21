01f57ad14531a8fec48212212dac8e8d3242b520
sorayaleon <soraya.l.g.88@gmail.com>
2022-03-21 21:45:27 +0100

    Add counter store into counter view

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Counter View</h1>
+  <h1>Counter View: {{counterStore.counter}}</h1>
+  <button @click="counterStore.incrementBy(1)">+1</button>
+  <button @click="counterStore.incrementBy(2)">+2</button>
+  <button @click="counterStore.incrementBy(5)">+5</button>
 </template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counter';
+
+const counterStore = useCounterStore();
+</script>
