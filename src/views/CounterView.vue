29d3e2d06e7b7512af840fd086575d43d8431695
sorayaleon <soraya.l.g.88@gmail.com>
2022-03-21 21:48:22 +0100

    Refactor incrementBy

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,12 +1,16 @@
 <template>
   <h1>Counter View: {{counterStore.counter}}</h1>
-  <button @click="counterStore.incrementBy(1)">+1</button>
-  <button @click="counterStore.incrementBy(2)">+2</button>
-  <button @click="counterStore.incrementBy(5)">+5</button>
+  <button @click="increaseBy(1)">+1</button>
+  <button @click="increaseBy(2)">+2</button>
+  <button @click="increaseBy(5)">+5</button>
 </template>
 
 <script lang="ts" setup>
 import { useCounterStore } from '../stores/counter';
 
 const counterStore = useCounterStore();
+
+const increaseBy = (value: number) => {
+  counterStore.incrementBy(value);
+}
 </script>
